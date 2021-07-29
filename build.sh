d897bfc1f6cc09380fdc10d1392228007e2fe827
Julien MIQUEL <julienmiquel@google.com>
2021-07-29 23:14:56 +0200

    update build.sh to includes var definitions

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-terraform destroy -auto-approve
-terraform apply -auto-approve -var="deletion_protection=false"
+echo replace project_ID by your GCP project_ID
+terraform destroy -auto-approve -var="env=test_lcbft_1" -var="project=project_ID"
+terraform apply -auto-approve -var="deletion_protection=false" -var="env=test_lcbft_1" -var="project=project_ID"
